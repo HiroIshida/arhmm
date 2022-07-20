22f2131b57ea3680318cb6fc56782cbf5db840fc
HiroIshida <spitfire.docomo@gmail.com>
2022-07-20 10:03:50 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = ["numpy", "scipy", 'dataclasses; python_version < "3.7"']
 
 setup(
     name="arhmm",
-    version="0.0.1",
+    version="0.0.2",
     description="",
     license="MIT",
     install_requires=install_requires,
