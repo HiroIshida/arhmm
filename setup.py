793082024de516a96de0e4464e9c140a96d007b2
HiroIshida <spitfire.docomo@gmail.com>
2022-07-21 13:51:28 +0900

    chore: fix setup.py for pypi release

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup_requires = []
 
@@ -6,9 +6,12 @@ install_requires = ["numpy", "scipy", 'dataclasses; python_version < "3.7"']
 
 setup(
     name="arhmm",
-    version="0.0.3",
+    version="0.0.4",
     description="",
+    author="Hirokazu Ishida",
+    author_email="h-ishida@jsk.imi.i.u-tokyo.ac.jp",
     license="MIT",
     install_requires=install_requires,
+    packages=find_packages(exclude=("tests", "docs")),
     package_data={"arhmm": ["py.typed"]},
 )
